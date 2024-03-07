792a57328f0a3bbba5a317ce8fb4fa9203136f93
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2024-03-06 23:49:49 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ constdb = require('./config/connection');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 
+const userRoutes = require('./routes/userRoutes');
+const thoughtRoutes = require('./routes/thoughtRoutes');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const cwd = process.cwd();
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+
 
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
